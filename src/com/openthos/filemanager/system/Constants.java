db5ceb0d6714de8f4564f337144022d9f74d9f4e
Chen Peng <0123hhtx@163.com>
2016-11-25 07:57:23 +0000

    OtoFileManager: Restore double click interval time to 1.0 seconds temporarily
    Also hide the feature of switch file view temporarily.
    
    Signed-off-by: Chen Peng <0123hhtx@163.com>
    Signed-off-by: Chen Gang <chengang@emindsoft.com.cn>

--- src/com/openthos/filemanager/system/Constants.java
+++ src/com/openthos/filemanager/system/Constants.java
@@ -16,7 +16,7 @@ public abstract class Constants {
     public static final String SYSTEMSPACEFRAGMENT_TAG = "systemspacefragment";
     public static final String SEARCHFRAGMENT_TAG = "searchfragment";
     public static final String ADDRESSFRAGMENT_TAG = "addressfragment";
-    public static final long DOUBLE_CLICK_INTERVAL_TIME = 300; // 0.3 second
+    public static final long DOUBLE_CLICK_INTERVAL_TIME = 1000; // 1.0 second
 
     public static final String LEFT_FAVORITES = "left_favorites";
     public static final String SYSTEM_SPACE_FRAGMENT = "system_space_fragment";
