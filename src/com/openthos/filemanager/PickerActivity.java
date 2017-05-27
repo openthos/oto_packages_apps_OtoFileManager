384acec433ee634acd912c558cc8cb8c0479aa53
Luo Junhuan <forward_m@126.com>
2017-05-27 11:28:35 +0000

    OtoFileManager: Add a feature that you can quick picker by pressing the key of ESC
    Signed-off-by: Luo Junhuan <forward_m@126.com>
    Signed-off-by: Wang Zhixu <potatomagic@163.com>

--- src/com/openthos/filemanager/PickerActivity.java
+++ src/com/openthos/filemanager/PickerActivity.java
@@ -1,9 +1,20 @@
 package com.openthos.filemanager;
 
+import android.app.Activity;
+
 /**
  * Created by root on 12/20/16.
  */
 
 public class PickerActivity extends MainActivity {
 
+    @Override
+    public void onBackPressed() {
+        if (mCurFragment == mSdStorageFragment) {
+             setResult(Activity.RESULT_OK);
+             finish();
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
