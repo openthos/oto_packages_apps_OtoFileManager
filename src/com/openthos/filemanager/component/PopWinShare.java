3b0b93d27724a582640e411f033f1c1764672994
Chen Peng <0123hhtx@163.com>
2017-06-16 08:53:44 +0000

    OtoFileManager: Keep menu styles of popupwindow and dialog consistent, also fix a bug(id:1366) and clean up the code
    Signed-off-by: Chen Peng <0123hhtx@163.com>
    Signed-off-by: Wang Zhixu <potatomagic@163.com>

--- src/com/openthos/filemanager/component/PopWinShare.java
+++ src/com/openthos/filemanager/component/PopWinShare.java
@@ -16,15 +16,12 @@ import java.util.List;
 public class PopWinShare extends PopupWindow {
     private static final String IV_SETTING_TAG = "iv_setting";
     private static final String IV_USB_TAG = "iv_usb";
-    private static final String IV_MENU_TAG = "iv_menu";
     private static final String MOUNT_POPWINDOW_TAG = "MOUNT_POPWINDOW_TAG";
 
     private View mMainView;
-    private TextView mPop_menu_refresh, mPop_menu_cancel_all, mPop_menu_copy,
-                     mPop_menu_delete, mPop_menu_send, mPop_menu_create, mPop_menu_exit,
-                     mPop_setting_view, mPop_usb_view, mPop_usb_info;
+    private TextView mPop_setting_view, mPop_usb_view, mPop_usb_info;
     private TextView mPopFormatUsb;
-    private LinearLayout mLl_menu, mLl_setting, mLl_usb;
+    private LinearLayout  mLl_setting, mLl_usb;
     private LinearLayout mLlMount;
     private TextView mPopMount, mPopUmount;
     private List<LinearLayout> mContainers;
@@ -33,37 +30,14 @@ public class PopWinShare extends PopupWindow {
                        int paramInt1, int paramInt2, String menu_tag) {
         super(mainActivity);
         mMainView = LayoutInflater.from(mainActivity).inflate(R.layout.popwin_share, null);
-        mLl_menu = (LinearLayout) mMainView.findViewById(R.id.ll_menu);
         mLl_setting = (LinearLayout) mMainView.findViewById(R.id.ll_setting);
         mLl_usb = (LinearLayout) mMainView.findViewById(R.id.ll_usb);
         mLlMount = (LinearLayout) mMainView.findViewById(R.id.ll_mount);
         mContainers = new ArrayList<>();
-        mContainers.add(mLl_menu);
         mContainers.add(mLl_setting);
         mContainers.add(mLl_usb);
         mContainers.add(mLlMount);
-        if (IV_MENU_TAG.equals(menu_tag)) {
-            mLl_setting.setVisibility(View.GONE);
-            mLl_usb.setVisibility(View.GONE);
-            mLl_menu.setVisibility(View.VISIBLE);
-            mPop_menu_refresh = (TextView) mMainView.findViewById(R.id.pop_menu_refresh);
-            mPop_menu_cancel_all = (TextView) mMainView.findViewById(R.id.pop_menu_cancel_all);
-            mPop_menu_copy = (TextView) mMainView.findViewById(R.id.pop_menu_copy);
-            mPop_menu_delete = (TextView) mMainView.findViewById(R.id.pop_menu_delete);
-            mPop_menu_send = (TextView) mMainView.findViewById(R.id.pop_menu_send);
-            mPop_menu_create = (TextView) mMainView.findViewById(R.id.pop_menu_create);
-            mPop_menu_exit = (TextView) mMainView.findViewById(R.id.pop_menu_exit);
-            if (paramOnClickListener != null) {
-                mPop_menu_refresh.setOnClickListener(paramOnClickListener);
-                mPop_menu_cancel_all.setOnClickListener(paramOnClickListener);
-                mPop_menu_copy.setOnClickListener(paramOnClickListener);
-                mPop_menu_delete.setOnClickListener(paramOnClickListener);
-                mPop_menu_send.setOnClickListener(paramOnClickListener);
-                mPop_menu_create.setOnClickListener(paramOnClickListener);
-                mPop_menu_exit.setOnClickListener(paramOnClickListener);
-            }
-        } else if (IV_SETTING_TAG.equals(menu_tag)) {
-            mLl_menu.setVisibility(View.GONE);
+        if (IV_SETTING_TAG.equals(menu_tag)) {
             mLl_usb.setVisibility(View.GONE);
             mLl_setting.setVisibility(View.VISIBLE);
             mPop_setting_view = (TextView) mMainView.findViewById(R.id.pop_setting_view);
@@ -71,7 +45,6 @@ public class PopWinShare extends PopupWindow {
                 mPop_setting_view.setOnClickListener(paramOnClickListener);
             }
         } else if (IV_USB_TAG.equals(menu_tag)) {
-            mLl_menu.setVisibility(View.GONE);
             mLl_setting.setVisibility(View.GONE);
             mLl_usb.setVisibility(View.VISIBLE);
             mPop_usb_view = (TextView) mMainView.findViewById(R.id.pop_usb_view);
@@ -84,7 +57,6 @@ public class PopWinShare extends PopupWindow {
             }
         }  else if (MOUNT_POPWINDOW_TAG.equals(menu_tag)) {
             mLlMount.setVisibility(View.VISIBLE);
-            mLl_menu.setVisibility(View.GONE);
             mLl_setting.setVisibility(View.GONE);
             mLl_usb.setVisibility(View.GONE);
             mPopMount = (TextView) mMainView.findViewById(R.id.pop_mount);
