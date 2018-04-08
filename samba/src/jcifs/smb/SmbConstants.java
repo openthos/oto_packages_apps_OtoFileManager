05afe72c942414cb92b7171366300b0b07325a00
Wang Zhixu <wangzhixu@openthos.org>
2018-04-08 06:52:41 +0000

    OtoFileManager: The samba connection timeout time is changed from 30 sec to 5 sec
    Signed-off-by: Wang Zhixu <wangzhixu@openthos.org>

--- samba/src/jcifs/smb/SmbConstants.java
+++ samba/src/jcifs/smb/SmbConstants.java
@@ -10,12 +10,12 @@ interface SmbConstants {
     static final int DEFAULT_PORT = 445;
 
     static final int DEFAULT_MAX_MPX_COUNT = 10;
-    static final int DEFAULT_RESPONSE_TIMEOUT = 30000;
-    static final int DEFAULT_SO_TIMEOUT = 35000;
+    static final int DEFAULT_RESPONSE_TIMEOUT = 5000;
+    static final int DEFAULT_SO_TIMEOUT = 5000;
     static final int DEFAULT_RCV_BUF_SIZE = 60416;
     static final int DEFAULT_SND_BUF_SIZE = 16644;
     static final int DEFAULT_SSN_LIMIT = 250;
-    static final int DEFAULT_CONN_TIMEOUT = 35000;
+    static final int DEFAULT_CONN_TIMEOUT = 5000;
 
     static final InetAddress LADDR = Config.getLocalHost();
     static final int LPORT = Config.getInt( "jcifs.smb.client.lport", 0 );
@@ -100,12 +100,12 @@ interface SmbConstants {
 
 
     // flags for move and copy
-    static final int FLAGS_TARGET_MUST_BE_FILE         = 0x0001; 
-    static final int FLAGS_TARGET_MUST_BE_DIRECTORY    = 0x0002; 
-    static final int FLAGS_COPY_TARGET_MODE_ASCII      = 0x0004; 
+    static final int FLAGS_TARGET_MUST_BE_FILE         = 0x0001;
+    static final int FLAGS_TARGET_MUST_BE_DIRECTORY    = 0x0002;
+    static final int FLAGS_COPY_TARGET_MODE_ASCII      = 0x0004;
     static final int FLAGS_COPY_SOURCE_MODE_ASCII      = 0x0008;
-    static final int FLAGS_VERIFY_ALL_WRITES           = 0x0010; 
-    static final int FLAGS_TREE_COPY                   = 0x0020; 
+    static final int FLAGS_VERIFY_ALL_WRITES           = 0x0010;
+    static final int FLAGS_TREE_COPY                   = 0x0020;
 
     // open function
     static final int OPEN_FUNCTION_FAIL_IF_EXISTS      = 0x0000;
